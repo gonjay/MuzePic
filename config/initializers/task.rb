8ec22ebbfdd47b5d927facd4b0ee6ddaa21a791d
gonjay <assyer@gmail.com>
2014-04-22 13:17:49 +0800

    Change time

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -1,21 +1,14 @@
 #!/bin/env ruby  
 # encoding: utf-8
 # require 'rubygems'
-require 'open-uri'
 require 'nokogiri'
 require 'rest-client'
 require 'mini_magick'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.every("30s") do
+scheduler.every("10m") do
   execFetch
-  begin
-   # execFetch
-  rescue Exception => e
-    puts e
-  end
-  
 end
 
 def execFetch
