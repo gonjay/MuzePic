ad7ad5d950d26b75f82ae7a153903700e41377a5
gonjay <assyer@gmail.com>
2014-04-23 09:26:38 +0800

    begin rescue

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -8,7 +8,13 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
 scheduler.every("10m") do
-  execFetch
+  
+  begin
+    execFetch
+  rescue Exception => e
+    puts e
+  end
+  
 end
 
 def execFetch
